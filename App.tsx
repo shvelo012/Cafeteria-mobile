8748a9c246669fbffb30145ffd7d71dc00c87b8f
NikolosD <alcari2002@gmail.com>
2023-10-10 13:51:10 +0400

    fixed app tsx

--- App.tsx
+++ App.tsx
@@ -1,41 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
 import MainContainer from './src/MainContainer';
 
-
-const [data, setData] = useState<any>();
-
 const apiUrl = 'http://192.168.0.113:3000';
-// Function to fetch all food items
-function getAllFoodItems() {
-  // Make a GET request to the /food/all endpoint
-  fetch(`${apiUrl}/food/all`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // Handle the data (an array of food items)
-      console.log(data);
-      setData(data);
-      // You can now display the data in your frontend
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
 
-// Call the function to fetch all food items
-// getAllFoodItems();
 const App: React.FC = () => {
+  const [data, setData] = useState<any>(null);
+
+  // Function to fetch all food items
+  const getAllFoodItems = () => {
+    // Make a GET request to the /food/all endpoint
+    fetch(`${apiUrl}/food/all`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // Handle the data (an array of food items)
+          console.log(data);
+          setData(data);
+          // You can now display the data in your frontend
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+  }
+
+  // Call the function to fetch all food items when the component mounts
+  useEffect(() => {
+    getAllFoodItems();
+  }, []); // Empty dependency array to run this effect only once on component mount
 
   return (
-    <View>
-      <MainContainer />
-    </View>
+      <View style={styles.container}>
+        <MainContainer />
+      </View>
   );
 };
 
@@ -47,3 +48,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
