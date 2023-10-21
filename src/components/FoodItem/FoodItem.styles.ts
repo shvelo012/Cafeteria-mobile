ab4cc56d5b7bc9aa48225acd2928888b7e0d96a4
shvelo012 <shvelidzesaba09@gmail.com>
2023-10-21 14:18:13 +0400

    o

--- src/components/FoodItem/FoodItem.styles.ts
+++ src/components/FoodItem/FoodItem.styles.ts
@@ -1,9 +1,23 @@
 import { StyleSheet } from "react-native";
 import { scaled } from "../scaler";
+import * as Device from 'expo-device';
+import { DeviceType } from "expo-device";
+export const deviceType = Device.deviceType;
 
-export const styles = StyleSheet.create({
-  title: {
-    fontSize: scaled(22),
-    alignSelf: 'center',
-  },
-});
+export const styles = StyleSheet.create(
+  (deviceType === DeviceType.PHONE ?
+    {//მობილურის სტილები
+      title: {
+        fontSize: scaled(22),
+        alignSelf: 'center',
+      },
+    }
+    :
+    //პლანშეტის სტილები 
+    {
+      title: {
+        fontSize: scaled(22),
+        alignSelf: 'center',
+      },
+    }
+  ));
