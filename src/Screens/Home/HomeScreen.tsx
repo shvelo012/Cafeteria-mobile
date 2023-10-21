b268aa3108f355faeb1e6a740d70a0a49c1c5be7
NikolosD <alcari2002@gmail.com>
2023-10-21 20:06:28 +0400

    styled home screen

--- src/Screens/Home/HomeScreen.tsx
+++ src/Screens/Home/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { View, Text, ScrollView, TouchableHighlight, ActivityIndicator } from 'react-native'; // Import ActivityIndicator
+import { View, Text, ScrollView, TouchableHighlight, ActivityIndicator } from 'react-native';
 import { navigate } from '../../Navigation/utils';
 import { Screens } from '../screenConstants';
 import { styles } from './HomeScreen.styles';
@@ -10,60 +10,90 @@ import { FoodItemType } from '../../types.ts/FoodItemType';
 import { themeSpacing } from '../../components/spacer';
 import { observer } from 'mobx-react';
 import { useFoodStore } from '../../stores/FoodStore/FoodStore.Provider';
+import { colors } from "../../components/colors";
+import {scaled} from "../../components/scaler";
 
 const getFoodAPI = `http://${DeviceApi}:4000/food/all`;
 
 const fetchDataFunction = async () => {
-  try {
-    const response = await fetch(getFoodAPI);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+    try {
+        const response = await fetch(getFoodAPI);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw error;
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw error;
-  }
 };
 
 const HomeScreen: React.FC = observer(() => {
-  const foodStore = useFoodStore();
-  useEffect(() => foodStore.reset(), []);
+    const foodStore = useFoodStore();
+    useEffect(() => foodStore.reset(), []);
 
-  const { data, error, isLoading } = useQuery({ queryKey: ['food'], queryFn: fetchDataFunction });
+    const { data, error, isLoading } = useQuery({ queryKey: ['food'], queryFn: fetchDataFunction });
 
-  if (!data || isLoading || error) {
+    if (!data || isLoading || error) {
+        return (
+            <View>
+                <Text>Loading...</Text>
+                <ActivityIndicator />
+            </View>
+        );
+    }
+
+
+    const groupedItems: FoodItemType[][] = [];
+    for (let i = 0; i < data.data.length; i += 2) {
+        groupedItems.push([data.data[i], data.data[i + 1]]);
+    }
+
     return (
-      <View>
-        <Text>Loading...</Text>
-        <ActivityIndicator />
-      </View>
-    );
-  }
+        <>
 
-  return (
-    <>
-      <TouchableHighlight style={{ zIndex: 1, position: 'relative', borderWidth: 2 }}>
-        <Text style={styles.logInButton} onPress={() => navigate(Screens.LoginScreenName)}>
-          Log In
-        </Text>
-      </TouchableHighlight>
-      <ScrollView>
-        <View
-          style={{
-            marginTop: themeSpacing(12),
-            width: '100%',
-            justifyContent: 'flex-start',
-            borderWidth: 2,
-          }}
-        >
-          {data && data.data.map((item: FoodItemType) => (
-            <FoodItem key={item.ID} info={item} />
-          ))}
-        </View>
-      </ScrollView>
-    </>
-  );
+            <View style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                zIndex: 1
+            }}>
+                <TouchableHighlight>
+                    <Text style={styles.logInButton} onPress={() => navigate(Screens.LoginScreenName)}>
+                        Log In
+                    </Text>
+                </TouchableHighlight>
+            </View>
+
+
+            <ScrollView style={{paddingTop: scaled(30)}}>
+                <View
+                    style={{
+                        marginTop: themeSpacing(12),
+                        width: '100%',
+                        justifyContent: 'flex-start',
+                        backgroundColor: colors.appBackground
+                    }}
+                >
+                    {groupedItems.map((itemPair: FoodItemType[], index: number) => (
+                        <View
+                            key={index}
+                            style={{
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                                marginBottom: themeSpacing(4),
+                            }}
+                        >
+                            {itemPair.map((item: FoodItemType) => (
+                                <FoodItem key={item.ID} info={item} />
+                            ))}
+                        </View>
+                    ))}
+                </View>
+            </ScrollView>
+        </>
+    );
 });
 
 export default HomeScreen;
--- src/components/FoodItem/FoodItem.styles.ts
+++ src/components/FoodItem/FoodItem.styles.ts
@@ -1,23 +1,53 @@
-import { StyleSheet } from "react-native";
-import { scaled } from "../scaler";
+import {StyleSheet} from "react-native";
+import {scaled} from "../scaler";
 import * as Device from 'expo-device';
-import { DeviceType } from "expo-device";
+import {DeviceType} from "expo-device";
+import {themeSpacing} from "../spacer";
+import {colors} from "../colors";
+
 export const deviceType = Device.deviceType;
 
 export const styles = StyleSheet.create(
-  (deviceType === DeviceType.PHONE ?
-    {//მობილურის სტილები
-      title: {
-        fontSize: scaled(22),
-        alignSelf: 'center',
-      },
-    }
-    :
-    //პლანშეტის სტილები 
-    {
-      title: {
-        fontSize: scaled(22),
-        alignSelf: 'center',
-      },
-    }
-  ));
+    (deviceType === DeviceType.PHONE ?
+            {//მობილურის სტილები
+                foodContainer:{
+                    borderWidth: 1.5,
+                    width: scaled(135),
+                    borderColor: colors.red,
+                    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+                    borderRadius: scaled(10),
+                    gap: themeSpacing(1),
+                },
+                title: {
+                    fontSize: scaled(20),
+                    marginLeft: themeSpacing(2),
+                    marginRight: themeSpacing(2),
+                    alignSelf: 'flex-start',
+                    fontWeight: 'bold'
+                },
+                foodImage: {
+                    width: scaled(63),
+                    height: scaled(63),
+                    alignSelf: 'center',
+                    padding: themeSpacing(7),
+
+                },
+                price:{
+                    marginLeft: themeSpacing(2),
+                    fontWeight: '500',
+                },
+                quantity:{
+                    marginLeft: themeSpacing(2),
+                    fontWeight: '500',
+                    marginBottom: themeSpacing(2),
+                }
+            }
+            :
+            //პლანშეტის სტილები
+            {
+                title: {
+                    fontSize: scaled(22),
+                    alignSelf: 'center',
+                },
+            }
+    ));
